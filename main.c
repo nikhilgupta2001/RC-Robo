0e98e38f1b862745e21fbdf587e119fb1550ac47
nikhilgupta2001 <56764834+nikhilgupta2001@users.noreply.github.com>
2020-01-28 23:03:53 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,18 @@
+/*
+ * rc_robo.c
+ *
+ * Created: 25-01-2020 12:43:34
+ * Author : HP PC
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
